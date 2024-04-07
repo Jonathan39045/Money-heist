318952294b611e3a383b6e02363293a8a6fc4e7a
Jonathan39045 <lonelover45@tutanota.com>
2024-04-07 12:17:54 +0530

    first commit

---
+++ all.js
@@ -0,0 +1,17 @@
+//Javascript data types:
+// numbers , strings , arrays , object etc...
+
+// To create a variable there is an keyword called "var".
+  // here it comes the variable types:
+  // numbers , dollarsign , letters, underscore..
+
+  // important note:
+  // javascript is case sensitive.. html is not case sensitive..
+
+
+  //styles of variables..
+   // partial case , camelcase , underscore case..
+
+//    var no=[12,45,23,78,"ajith","ganapathy"]
+//    no.push("google")
+//    alert(no.length);
